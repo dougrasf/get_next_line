c051aefd1fa7db8e1b41dc4e69f257a156be0e7d
dofranci <dofranci@student.42sp.org.br>
2022-07-07 21:34:15 -0300

    get_next_line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dofranci <dofranci@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 22:50:55 by dofranci          #+#    #+#             */
+/*   Updated: 2022/07/07 18:48:19 by dofranci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char    *get_conteudo(int fd, char *conteudo);
+char    *get_next_line(int fd);
+
+#endif
---
+++ get_next_line_bonus_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dofranci <dofranci@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 22:35:15 by dofranci          #+#    #+#             */
+/*   Updated: 2022/07/07 20:14:12 by dofranci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	index;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	if ((char)c == '\0')
+		return (0);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*dst;
+	int		x;
+	int		y;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (dst == NULL)
+		return (NULL);
+	x = 0;
+	while (s1[x] != '\0')
+	{
+		dst[x] = s1[x];
+		x++;
+	}
+	y = 0;
+	while (s2[y] != '\0')
+	{
+		dst[x + y] = s2[y];
+		y++;
+	}
+	dst[x + y] = '\0';
+	free(s1);
+	return (dst);
+}
